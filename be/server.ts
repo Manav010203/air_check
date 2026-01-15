5c909dbfa89a87d61e27b79a6f0cf6f09cf4028e
Manav010203 <manav01022003@gmail.com>
2026-01-15 22:25:38 +0530

    need update the link

--- be/server.ts
+++ be/server.ts
@@ -6,7 +6,16 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get()
+app.get("/",async(req,res)=>{
+    try{
+    const data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=amritsar&appid=4425ff7391b0e6d15b651d74508d16e3")
+    return res.json({data});
+    }catch(e){
+        console.log(e);
+        return res.status(401).json("Something went wrong in fetching data");
+    }
+
+})
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
