c026da771c88df51be47776fd6d5ec190592742e
Manav010203 <manav01022003@gmail.com>
2026-01-15 20:48:21 +0530

    server

---
+++ be/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get()
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
